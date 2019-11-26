d919b9404c06b8e3a370f6fe133b9f7560446924
cbivens1914 <58235567+cbivens1914@users.noreply.github.com>
2019-11-26 14:39:00 -0800

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,3 +1,18 @@
 //CS135.1002
 //NetIds: ajara, 
 //Purpose: A simplified game of memory
+
+#include <stdio.h>
+
+//maybe a #define or two
+
+//lot of function prototypes here
+
+int main () {
+  //declare variables
+  
+  
+ return 0; 
+}
+
+//lot of fuckin function definitions
