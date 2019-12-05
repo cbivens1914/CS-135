5c073839f91e322604470ee979a2c84f41147e85
AlyciaJ <58235966+AlyciaJ@users.noreply.github.com>
2019-12-05 12:44:54 -0800

    Update memory.c

--- memory.c
+++ memory.c
@@ -10,7 +10,38 @@
 
 int main () {
   //declare variables
-  
+  int menu, difficulty;
+
+  //menu
+  do
+  {
+    printf("***MEMORY!***\n");
+    printf("1 - Play Game\n");
+    printf("2 - Check Scores\n");
+    printf("0 - Exit\n");
+    scanf("%d", &menu);
+
+    switch(menu)
+    {
+	//Play Game
+	case 1: printf("Enter difficulty (1, 2, or 3): ");
+		scanf("%d", &difficulty);
+		//game
+		break;
+
+	//Check Scores
+	case 2: printf("**HIGH SCORES**\n");
+		//display top 10 scores
+		printf("\n");
+		break;
+
+	//Exit
+	case 0: break;
+
+	//Other
+	default: printf("Please enter a valid menu option.\n\n");
+		break;
+    }
   
  return 0; 
 }
